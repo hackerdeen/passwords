2920edb03fe827b532b4dad4cb278669b09074ec
Robert McWilliam <rmcw@allmail.net>
2015-06-17 15:16:01 +0100

    Initial setup - only test data in so far.

---
+++ encrypt.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+hibby=1904771B
+tom=4E1E907C
+ed=60DBF10E
+robert=9DCA7AA9
+carl=003BE2C0
+
+gpg --recipient $hibby --recipient $tom --recipient $ed --recipient $robert --recipient $carl --encrypt-files passwords
