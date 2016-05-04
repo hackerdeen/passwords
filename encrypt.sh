55873d815dee113c633d5d5a91df7f8bcf968215
Robert McWilliam <rmcw@allmail.net>
2016-05-04 21:53:38 +0100

    Update to replace Carl with Kevin for change of director (and set the right key for me).

--- encrypt.sh
+++ encrypt.sh
@@ -3,8 +3,8 @@
 hibby=1904771B
 tom=4E1E907C
 ed=60DBF10E
-robert=9DCA7AA9
-carl=003BE2C0
+robert=6D003582
+kev=6153AAA8
 
-gpg -r $hibby -r $tom -r $ed -r $robert -r $carl --encrypt-files passwords
+gpg -r $hibby -r $tom -r $ed -r $robert -r $kev --encrypt-files passwords
 gpg --armour --detach-sign passwords.gpg
