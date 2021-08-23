17b9df49dc2d28495ebf683fe198d4aa23d29928
Dave Hibberd <d@vehibberd.com>
2021-08-23 10:52:50 +0100

    Retire Kev, Ed and Rob from password store. Add irl. Add Bitfolk.

--- encrypt.sh
+++ encrypt.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-hibby=1904771B
-tom=4E1E907C
-ed=60DBF10E
-robert=6D003582
-kev=6153AAA8
+hibby=5E4F545193E8969C8A59B80A03A1FB7A1904771B
+tom=E6A646E485D52387AA96D0A537AD5E024E1E907C
+#ed=60DBF10E
+#robert=6D003582
+#kev=6153AAA8
+irl=A8F7BA5041E133339CBA169676D58093F540ABCD
 
-gpg -r $hibby -r $tom -r $ed -r $robert -r $kev --encrypt-files passwords
+gpg -r $hibby -r $tom -r $irl --encrypt-files passwords
 gpg --armour --detach-sign passwords.gpg
